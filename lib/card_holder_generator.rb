d126ba9616a7173f2b77e8adcd9a4eec2c95f9dc
Carlos Paramio <hola@carlosparamio.com>
2018-03-29 12:15:02 +0200

    Optional magnets holes

--- lib/card_holder_generator.rb
+++ lib/card_holder_generator.rb
@@ -18,6 +18,7 @@ difference() {
   #{main_cube}
   #{main_hole}
   #{gaps}
+  #{magnets if magnets?}
 }
     SCAD
   end
@@ -33,6 +34,8 @@ private
     cards_width
     cards_height
     gap_depths
+    magnets_height
+    magnets_diameter
   ).each do |setting_name|
     define_method(setting_name) do
       settings[setting_name]
@@ -44,7 +47,7 @@ private
   end
 
   def external_y
-    gap_depths.inject(&:+) + (gap_depths.size + 1) * separators_depth
+    external_walls_depth * 2 + gap_depths.inject(&:+) + (gap_depths.size - 1) * separators_depth
   end
 
   def external_z
@@ -77,7 +80,7 @@ module roundedcube(xdim, ydim, zdim, rdim) {
 
   def main_hole
     <<-MAINHOLE
-translate([#{external_walls_depth + separators_width}, #{separators_depth}, #{floor_depth}]) mirror([0, 1, 0]) rotate([90, 0, 0]) roundedcube(#{cards_height - separators_width * 2}, #{external_z * 2}, #{external_y - 2 * separators_depth}, 10);
+translate([#{external_walls_depth + separators_width}, #{external_walls_depth}, #{floor_depth}]) mirror([0, 1, 0]) rotate([90, 0, 0]) roundedcube(#{cards_height - separators_width * 2}, #{external_z * 2}, #{external_y - 2 * external_walls_depth}, 10);
 translate([#{external_walls_depth + separators_width}, 0, #{floor_depth + cards_width * 0.5}]) mirror([0, 1, 0]) rotate([90, 0, 0]) roundedcube(#{cards_height - separators_width * 2}, #{external_z * 2}, #{separators_width}, 10);
 translate([#{external_walls_depth + separators_width}, #{external_y - separators_width}, #{floor_depth + cards_width * 0.5}]) mirror([0, 1, 0]) rotate([90, 0, 0]) roundedcube(#{cards_height - separators_width * 2}, #{external_z * 2}, #{separators_width + 1}, 10);
     MAINHOLE
@@ -87,11 +90,22 @@ translate([#{external_walls_depth + separators_width}, #{external_y - separators
     gaps = ""
     gap_subtotal = 0
     gap_depths.each_with_index do |gap_depth, index|
-      gaps += "translate([#{external_walls_depth}, #{separators_depth * (index + 1) + gap_subtotal}, #{floor_depth}]) cube([#{cards_height}, #{gap_depth}, #{external_z}]); "
-      gaps += "translate([#{external_walls_depth + separators_width}, #{separators_depth * (index + 1) + gap_subtotal}, 0]) cube([#{cards_height - separators_width * 2}, #{gap_depth}, #{external_z}]); "
+      gaps += "translate([#{external_walls_depth}, #{external_walls_depth + separators_depth * index + gap_subtotal}, #{floor_depth}]) cube([#{cards_height}, #{gap_depth}, #{external_z}]); "
+      gaps += "translate([#{external_walls_depth + separators_width}, #{external_walls_depth + separators_depth * index + gap_subtotal}, 0]) cube([#{cards_height - separators_width * 2}, #{gap_depth}, #{external_z}]); "
       gap_subtotal += gap_depth
     end
     gaps
+  end
+
+  def magnets
+    "translate([#{external_walls_depth / 2}, #{external_walls_depth / 2}, #{external_z - magnets_height + 1}]) cylinder(#{magnets_height}, d = #{magnets_diameter}, $fn = 100);" +
+    "translate([#{external_x - external_walls_depth / 2}, #{external_walls_depth / 2}, #{external_z - magnets_height + 1}]) cylinder(#{magnets_height}, d = #{magnets_diameter}, $fn = 100);" +
+    "translate([#{external_walls_depth / 2}, #{external_y - external_walls_depth / 2}, #{external_z - magnets_height + 1}]) cylinder(#{magnets_height}, d = #{magnets_diameter}, $fn = 100);" +
+    "translate([#{external_x - external_walls_depth / 2}, #{external_y - external_walls_depth / 2}, #{external_z - magnets_height + 1}]) cylinder(#{magnets_height}, d = #{magnets_diameter}, $fn = 100);"
+  end
+
+  def magnets?
+    magnets_height > 0 && magnets_diameter > 0
   end
 
 end
